9fd30a469b39e3876de9bd681724e8e0fbd07830
kaede0902 <kaede0902js@gmail.com>
2019-02-06 16:39:03 +0900

    add hours

---
+++ FirstJS/ch03/132.js
@@ -0,0 +1,8 @@
+const a4 = [
+    {name: 'Ruby', hardness: 9},
+    {name: 'Diamond', hardness: 10},
+    {name: 'Topaz', hardness: 8},
+];
+console.log(a4[1].name);
+console.log(a4[2].hardness);
+
---
+++ FirstJS/ch03/152.js
@@ -0,0 +1,11 @@
+const halloween = new Date(2016,9,31);// 10-30
+console.log(halloween);
+
+const halloweenParty = new Date(2016,9,31,19,0);// 19:00
+console.log(halloweenParty);
+
+console.log(halloweenParty.getMonth());
+console.log(halloweenParty.getDate());
+console.log(halloweenParty.getDay());
+console.log(halloweenParty.getHours());
+
