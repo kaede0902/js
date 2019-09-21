c49036a50dfd9ea8587f0d9f2f0ea61994131c89
kaede0902 <kaede0920js@gmail.com>
2019-09-21 19:48:05 +0900

    add tko

--- works/definePhoneNum/src.js
+++ works/definePhoneNum/src.js
@@ -1,12 +1,14 @@
 let reg_ykhm = /045-\d{3}-\d{4}$/;
+let reg_tko = /03-\d{4}-\d{4}$/;
 
 let form = document.getElementById('form');
 form.onsubmit = () => {
     let val = form.hoge.value;
     if (reg_ykhm.test(val))
         alert(`${val} : Yokohama`);
+    else if (reg_tko.test(val))
+        alert(`${val} : Tokyo`);
     else 
-        alert(`${val} : Not Yokohama`);
+        alert(`${val} : Not Yokohama or Tokyo`);
     return false;
 }
-let reg_tko = /03-\d{4}-\d{4}/;
